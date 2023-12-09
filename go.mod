96ab3b23c7459a46e88ec9f496f9ce28d5755ca4
Eyo Chen <eyo.chen@amazingtalker.com>
2023-12-09 22:43:31 +0800

    feat(usecase): add Signup

--- go.mod
+++ go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/joho/godotenv v1.5.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
+	golang.org/x/crypto v0.16.0 // indirect
 )
---
+++ internel/usecase/user.go
@@ -0,0 +1,46 @@
+package usecase
+
+import (
+	"github.com/OYE0303/expense-tracker-go/internel/domain"
+	"github.com/OYE0303/expense-tracker-go/pkg/auth"
+	"github.com/OYE0303/expense-tracker-go/pkg/logger"
+)
+
+type userUC struct {
+	User UserModel
+}
+
+func newUserUC(UserModel UserModel) *userUC {
+	return &userUC{User: UserModel}
+}
+
+type Claims struct {
+	UserID    int64  `json:"user_id"`
+	UserName  string `json:"user_name"`
+	UserEmail string `json:"user_email"`
+}
+
+func (u *userUC) Signup(user *domain.User) error {
+	userByEmail, err := u.User.FindByEmail(user.Email)
+	if err != nil {
+		logger.Error("u.User.FindByEmail failed", "package", "usecase", "err", err)
+		return err
+	}
+
+	if userByEmail != nil {
+		return domain.ErrDataAlreadyExists
+	}
+
+	passwordHash, err := auth.GenerateHashPassword(user.Password)
+	if err != nil {
+		logger.Error("auth.GenerateHashPassword failed", "package", "usecase", "err", err)
+		return err
+	}
+
+	if err := u.User.Create(user.Name, user.Email, passwordHash, user.CountryID); err != nil {
+		logger.Error("u.User.Insert failed", "package", "usecase", "err", err)
+		return err
+	}
+
+	return nil
+}
