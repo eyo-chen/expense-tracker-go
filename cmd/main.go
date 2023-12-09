562aaf4e105934ea0f6b695d379d3c12bc1cb90b
Eyo Chen <eyo.chen@amazingtalker.com>
2023-12-09 22:28:58 +0800

    feat: connect to DB, and server

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/OYE0303/expense-tracker-go/pkg/logger"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	logger.Register()
+	if err := godotenv.Load(); err != nil {
+		logger.Fatal("Error loading .env file", "error", err)
+	}
+
+	logger.Info("Connecting to database...")
+	db, err := openDB()
+	if err != nil {
+		logger.Fatal("Unable to connect to database", "error", err)
+	}
+	defer db.Close()
+
+	if err := initServe(); err != nil {
+		logger.Fatal("Unable to start server", "error", err)
+	}
+}
+
+func openDB() (*sql.DB, error) {
+	config := map[string]string{
+		"name":     os.Getenv("DB_NAME"),
+		"user":     os.Getenv("DB_USER"),
+		"password": os.Getenv("DB_PASSWORD"),
+	}
+
+	dsn := fmt.Sprintf("%s:%s@tcp(127.0.0.1:54321)/%s?parseTime=true", config["user"], config["password"], config["name"])
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initServe() error {
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", 4000),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	logger.Info("Starting server", "addr", srv.Addr)
+	if err := srv.ListenAndServe(); err != nil {
+		return err
+	}
+
+	return nil
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/OYE0303/expense-tracker-go
 go 1.21.4
 
 require (
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.26.0 // indirect
 )
