1f88f1a0d554b414b9342117caf4bc2d5546e35d
Eyo Chen <eyo.chen@amazingtalker.com>
2023-12-16 00:19:55 +0800

    feat(handler): add Add main category

--- internal/handler/interface.go
+++ internal/handler/interface.go
@@ -2,9 +2,17 @@ package handler
 
 import "github.com/OYE0303/expense-tracker-go/internal/domain"
 
+// UserUC is the interface that wraps the basic methods for user usecase.
 type UserUC interface {
 	// Signup is a function that registers a user.
 	Signup(user *domain.User) error
 
+	// Login is a function that logs in a user.
 	Login(user *domain.User) (string, error)
+}
+
+// MainCategUC is the interface that wraps the basic methods for main category usecase.
+type MainCategUC interface {
+	// Add is a function that adds a main category.
+	Add(categ *domain.MainCateg, userID int64, iconID int64) error
 }
---
+++ internal/handler/maincateg.go
@@ -0,0 +1,63 @@
+package handler
+
+import (
+	"net/http"
+
+	"github.com/OYE0303/expense-tracker-go/internal/domain"
+	"github.com/OYE0303/expense-tracker-go/pkg/ctxutil"
+	"github.com/OYE0303/expense-tracker-go/pkg/errutil"
+	"github.com/OYE0303/expense-tracker-go/pkg/jsutil"
+	"github.com/OYE0303/expense-tracker-go/pkg/logger"
+	"github.com/OYE0303/expense-tracker-go/pkg/validator"
+)
+
+type mainCategHandler struct {
+	MainCateg MainCategUC
+}
+
+func newMainCategHandler(m MainCategUC) *mainCategHandler {
+	return &mainCategHandler{MainCateg: m}
+}
+
+func (m *mainCategHandler) AddMainCateg(w http.ResponseWriter, r *http.Request) {
+	var input struct {
+		Name   string `json:"name"`
+		Type   string `json:"type"`
+		IconID int64  `json:"icon_id"`
+	}
+	if err := jsutil.ReadJson(w, r, &input); err != nil {
+		logger.Error("jsutil.ReadJSON failed", "package", "handler", "err", err)
+		errutil.BadRequestResponse(w, r, err)
+		return
+	}
+
+	categ := domain.MainCateg{
+		Name:   input.Name,
+		Type:   input.Type,
+		IconID: input.IconID,
+	}
+
+	v := validator.New()
+	if !v.AddMainCateg(&categ) {
+		errutil.VildateErrorResponse(w, r, v.Error)
+		return
+	}
+
+	user := ctxutil.GetUser(r)
+	if err := m.MainCateg.Add(&categ, user.ID, categ.IconID); err != nil {
+		if err == domain.ErrDataAlreadyExists {
+			errutil.BadRequestResponse(w, r, err)
+			return
+		}
+
+		logger.Error("m.MainCateg.Add failed", "package", "handler", "err", err)
+		errutil.ServerErrorResponse(w, r, err)
+		return
+	}
+
+	if err := jsutil.WriteJSON(w, http.StatusCreated, nil, nil); err != nil {
+		logger.Error("jsutil.WriteJSON failed", "package", "handler", "err", err)
+		errutil.ServerErrorResponse(w, r, err)
+		return
+	}
+}
