c3cc5005a5655eefe71013a90ae7e287e163411a
Eyo Chen <eyo.chen@amazingtalker.com>
2023-12-31 21:33:24 +0800

    feat(domain): add TransactionResp

--- internal/domain/transaction.go
+++ internal/domain/transaction.go
@@ -3,7 +3,7 @@ package domain
 import "time"
 
 type Transaction struct {
-	ID          int64      `json:"id"`
+	ID          string     `json:"id"`
 	UserID      int64      `json:"user_id"`
 	Type        string     `json:"type"`
 	MainCategID int64      `json:"main_category_id"`
@@ -11,4 +11,16 @@ type Transaction struct {
 	Price       int64      `json:"price"`
 	Date        *time.Time `json:"date"`
 	Note        string     `json:"note"`
+}
+
+type GetQuery struct {
+	StartDate string `json:"start_date"`
+	EndDate   string `json:"end_date"`
+}
+
+type TransactionResp struct {
+	Transactions []*Transaction `json:"transactions"`
+	Income       int64          `json:"income"`
+	Expense      int64          `json:"expense"`
+	NetIncome    int64          `json:"net_income"`
 }
