090d7bec3dc66f42a33d170798fc2552f9f65e84
Eyo Chen <eyo.chen@amazingtalker.com>
2024-01-27 01:02:14 +0800

    feat: add comment at domain error

--- internal/domain/error.go
+++ internal/domain/error.go
@@ -3,22 +3,36 @@ package domain
 import "errors"
 
 var (
-	ErrDataAlreadyExists    = errors.New("data already exists")
-	ErrDataNotFound         = errors.New("data not found")
-	ErrAuthentication       = errors.New("authentication failed")
-	ErrAuthToken            = errors.New("invalid auth token")
-	ErrServer               = errors.New("internal server error")
-	ErrInvalidMainCategType = errors.New("invalid main category type")
+	// data already exists error
+	ErrDataAlreadyExists = errors.New("data already exists")
 
-	// main category
-	ErrMainCategNotFound  = errors.New("main category not found")
-	ErrUniqueIconUser     = errors.New("icon already used by another main category")
+	// data not found error
+	ErrDataNotFound = errors.New("data not found")
+
+	// authentication error
+	ErrAuthentication = errors.New("authentication failed")
+
+	// authorization error
+	ErrAuthToken = errors.New("invalid auth token")
+
+	// internal server error
+	ErrServer = errors.New("internal server error")
+
+	// main category not found error
+	ErrMainCategNotFound = errors.New("main category not found")
+
+	// main category unique icon error
+	ErrUniqueIconUser = errors.New("icon already used by another main category")
+
+	// main category unique name error
 	ErrUniqueNameUserType = errors.New("name already used by another main category with the same type")
 
-	// sub category
-	ErrSubCategNotFound        = errors.New("sub category not found")
+	// sub category not found error
+	ErrSubCategNotFound = errors.New("sub category not found")
+
+	// sub category unique name error
 	ErrUniqueNameUserMainCateg = errors.New("name already used by another sub category with the same main category")
 
-	// icon
+	// icon not found error
 	ErrIconNotFound = errors.New("icon not found")
 )
