a8dfea58379babe08171ca805bc8e35eb02be674
Eyo Chen <eyo.chen@amazingtalker.com>
2024-02-04 00:28:44 +0800

    feat: use factory at main user model

---
+++ internal/model/user/factory.go
@@ -0,0 +1,31 @@
+package user
+
+import (
+	"database/sql"
+	"fmt"
+)
+
+func BluePrintUser(i int, last User) User {
+	return User{
+		Name:          "test" + fmt.Sprint(i),
+		Email:         "test" + fmt.Sprint(i) + "@gmail.com",
+		Password_hash: "test" + fmt.Sprint(i),
+	}
+}
+
+func InserterUser(db *sql.DB, u User) (User, error) {
+	stmt := `INSERT INTO users (name, email, password_hash) VALUES (?, ?, ?)`
+	res, err := db.Exec(stmt, u.Name, u.Email, u.Password_hash)
+	if err != nil {
+		return User{}, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return User{}, err
+	}
+
+	u.ID = id
+
+	return u, nil
+}
