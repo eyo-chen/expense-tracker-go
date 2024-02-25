4700901c50be0f7b6cdc99f9810f957d7046bca0
Eyo Chen <eyo.chen@amazingtalker.com>
2024-02-25 13:11:22 +0800

    feat: setup router

--- pkg/validator/transaction.go
+++ pkg/validator/transaction.go
@@ -20,7 +20,15 @@ func (v *Validator) CreateTransaction(t domain.CreateTransactionInput) bool {
 func (v *Validator) GetTransaction(q domain.GetQuery) bool {
 	v.Check(isValidDateFormat(q.StartDate), "startDate", "Start date must be in YYYY-MM-DD format")
 	v.Check(isValidDateFormat(q.EndDate), "endDate", "End date must be in YYYY-MM-DD format")
-	v.Check(checkStartDateBeforeEndDate(q), "startDate", "Start date must be before end date")
+	v.Check(checkStartDateBeforeEndDate(q.StartDate, q.EndDate), "startDate", "Start date must be before end date")
+
+	return v.Valid()
+}
+
+func (v *Validator) GetAccInfo(q domain.GetAccInfoQuery) bool {
+	v.Check(isValidDateFormat(q.StartDate), "startDate", "Start date must be in YYYY-MM-DD format")
+	v.Check(isValidDateFormat(q.EndDate), "endDate", "End date must be in YYYY-MM-DD format")
+	v.Check(checkStartDateBeforeEndDate(q.StartDate, q.EndDate), "startDate", "Start date must be before end date")
 
 	return v.Valid()
 }
@@ -34,12 +42,12 @@ func isValidDateFormat(dateString string) bool {
 	return err == nil
 }
 
-func checkStartDateBeforeEndDate(query domain.GetQuery) bool {
-	if query.StartDate == "" || query.EndDate == "" {
+func checkStartDateBeforeEndDate(startDate, endDate string) bool {
+	if startDate == "" || endDate == "" {
 		return true
 	}
 
-	startDate, _ := time.Parse(time.DateOnly, query.StartDate)
-	endDate, _ := time.Parse(time.DateOnly, query.EndDate)
-	return startDate.Before(endDate) || startDate.Equal(endDate)
+	start, _ := time.Parse(time.DateOnly, startDate)
+	end, _ := time.Parse(time.DateOnly, endDate)
+	return start.Before(end) || start.Equal(end)
 }
