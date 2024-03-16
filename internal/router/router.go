249f158eba989530f2934fc788237d72c61211fc
CHEN YI YOU <78901921+OYE0303@users.noreply.github.com>
2024-03-16 14:50:07 +0800

    feat: add delete transactions at handler and router (#42)
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

--- internal/router/router.go
+++ internal/router/router.go
@@ -39,6 +39,7 @@ func New(handler *hd.Handler) http.Handler {
 	r.Handle("/v1/transaction", auth.ThenFunc(handler.Transaction.Create)).Methods(http.MethodPost)
 	r.Handle("/v1/transaction", auth.ThenFunc(handler.Transaction.GetAll)).Methods(http.MethodGet)
 	r.Handle("/v1/transaction/info", auth.ThenFunc(handler.Transaction.GetAccInfo)).Methods(http.MethodGet)
+	r.Handle("/v1/transaction/{id}", auth.ThenFunc(handler.Transaction.Delete)).Methods(http.MethodDelete)
 
 	regular := alice.New(middleware.LogRequest, middleware.EnableCORS)
 
