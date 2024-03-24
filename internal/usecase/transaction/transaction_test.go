89f882fd26053e597f80932339b0c7b4e03a6bdf
Eyo Chen <eyo.chen@amazingtalker.com>
2024-03-24 19:51:11 +0800

    refactor: uncomment

--- internal/usecase/transaction/transaction_test.go
+++ internal/usecase/transaction/transaction_test.go
@@ -240,237 +240,237 @@ func update_UpdateFail_UpdateSuccessfully(s *TransactionSuite, desc string) {
 	s.Require().Equal(errors.New("error"), err, desc)
 }
 
-// func (s *TransactionSuite) TestDelete() {
-// 	for scenario, fn := range map[string]func(s *TransactionSuite, desc string){
-// 		"when no error, delete successfully":       delete_NoError_DeleteSuccessfully,
-// 		"when check permession fail, return error": delete_CheckPermessionFail_ReturnError,
-// 	} {
-// 		s.Run(testutil.GetFunName(fn), func() {
-// 			s.SetupTest()
-// 			fn(s, scenario)
-// 			s.TearDownTest()
-// 		})
-// 	}
-// }
+func (s *TransactionSuite) TestDelete() {
+	for scenario, fn := range map[string]func(s *TransactionSuite, desc string){
+		"when no error, delete successfully":       delete_NoError_DeleteSuccessfully,
+		"when check permession fail, return error": delete_CheckPermessionFail_ReturnError,
+	} {
+		s.Run(testutil.GetFunName(fn), func() {
+			s.SetupTest()
+			fn(s, scenario)
+			s.TearDownTest()
+		})
+	}
+}
 
-// func delete_NoError_DeleteSuccessfully(s *TransactionSuite, desc string) {
-// 	user := domain.User{
-// 		ID: 1,
-// 	}
+func delete_NoError_DeleteSuccessfully(s *TransactionSuite, desc string) {
+	user := domain.User{
+		ID: 1,
+	}
 
-// 	s.mockTransaction.
-// 		On("GetByIDAndUserID", mockCtx, int64(1), user.ID).
-// 		Return(domain.Transaction{}, nil).Once()
+	s.mockTransaction.
+		On("GetByIDAndUserID", mockCtx, int64(1), user.ID).
+		Return(domain.Transaction{}, nil).Once()
 
-// 	s.mockTransaction.On("Delete", mockCtx, int64(1)).
-// 		Return(nil).Once()
+	s.mockTransaction.On("Delete", mockCtx, int64(1)).
+		Return(nil).Once()
 
-// 	err := s.transactionUC.Delete(mockCtx, int64(1), user)
-// 	s.Require().NoError(err, desc)
-// }
+	err := s.transactionUC.Delete(mockCtx, int64(1), user)
+	s.Require().NoError(err, desc)
+}
 
-// func delete_CheckPermessionFail_ReturnError(s *TransactionSuite, desc string) {
-// 	user := domain.User{
-// 		ID: 1,
-// 	}
+func delete_CheckPermessionFail_ReturnError(s *TransactionSuite, desc string) {
+	user := domain.User{
+		ID: 1,
+	}
 
-// 	s.mockTransaction.
-// 		On("GetByIDAndUserID", mockCtx, int64(1), user.ID).
-// 		Return(domain.Transaction{}, errors.New("error")).Once()
+	s.mockTransaction.
+		On("GetByIDAndUserID", mockCtx, int64(1), user.ID).
+		Return(domain.Transaction{}, errors.New("error")).Once()
 
-// 	err := s.transactionUC.Delete(mockCtx, int64(1), user)
-// 	s.Require().Equal(errors.New("error"), err, desc)
-// }
+	err := s.transactionUC.Delete(mockCtx, int64(1), user)
+	s.Require().Equal(errors.New("error"), err, desc)
+}
 
-// func (s *TransactionSuite) TestGetBarChartData() {
-// 	tests := []struct {
-// 		desc            string
-// 		setupFun        func()
-// 		chartDateRange  domain.ChartDateRange
-// 		transactionType domain.TransactionType
-// 		user            domain.User
-// 		expResult       domain.ChartData
-// 		expErr          error
-// 	}{
-// 		{
-// 			desc: "when no error, return chart data",
-// 			setupFun: func() {
-// 				chartDataRange := domain.ChartDateRange{
-// 					StartDate: "2024-03-17",
-// 					EndDate:   "2024-03-23",
-// 				}
+func (s *TransactionSuite) TestGetBarChartData() {
+	tests := []struct {
+		desc            string
+		setupFun        func()
+		chartDateRange  domain.ChartDateRange
+		transactionType domain.TransactionType
+		user            domain.User
+		expResult       domain.ChartData
+		expErr          error
+	}{
+		{
+			desc: "when no error, return chart data",
+			setupFun: func() {
+				chartDataRange := domain.ChartDateRange{
+					StartDate: "2024-03-17",
+					EndDate:   "2024-03-23",
+				}
 
-// 				chartDataByWeekday := domain.ChartDataByWeekday{
-// 					"Sun": 100,
-// 					"Mon": 200,
-// 					"Tue": 300,
-// 					"Wed": 400,
-// 					"Thu": 500,
-// 					"Fri": 600,
-// 					"Sat": 700,
-// 				}
+				chartDataByWeekday := domain.ChartDataByWeekday{
+					"Sun": 100,
+					"Mon": 200,
+					"Tue": 300,
+					"Wed": 400,
+					"Thu": 500,
+					"Fri": 600,
+					"Sat": 700,
+				}
 
-// 				s.mockTransaction.On("GetBarChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
-// 					Return(chartDataByWeekday, nil).Once()
-// 			},
-// 			chartDateRange: domain.ChartDateRange{
-// 				StartDate: "2024-03-17",
-// 				EndDate:   "2024-03-23",
-// 			},
-// 			transactionType: domain.TransactionTypeExpense,
-// 			user: domain.User{
-// 				ID: 1,
-// 			},
-// 			expResult: domain.ChartData{
-// 				Labels:   []string{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
-// 				Datasets: []float64{100, 200, 300, 400, 500, 600, 700},
-// 			},
-// 			expErr: nil,
-// 		},
-// 		{
-// 			desc: "when chart data by weekday is not fully filled, still return chart data",
-// 			setupFun: func() {
-// 				chartDataRange := domain.ChartDateRange{
-// 					StartDate: "2024-03-17",
-// 					EndDate:   "2024-03-23",
-// 				}
+				s.mockTransaction.On("GetBarChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
+					Return(chartDataByWeekday, nil).Once()
+			},
+			chartDateRange: domain.ChartDateRange{
+				StartDate: "2024-03-17",
+				EndDate:   "2024-03-23",
+			},
+			transactionType: domain.TransactionTypeExpense,
+			user: domain.User{
+				ID: 1,
+			},
+			expResult: domain.ChartData{
+				Labels:   []string{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
+				Datasets: []float64{100, 200, 300, 400, 500, 600, 700},
+			},
+			expErr: nil,
+		},
+		{
+			desc: "when chart data by weekday is not fully filled, still return chart data",
+			setupFun: func() {
+				chartDataRange := domain.ChartDateRange{
+					StartDate: "2024-03-17",
+					EndDate:   "2024-03-23",
+				}
 
-// 				// only have data for Sun, Mon, Tue
-// 				chartDataByWeekday := domain.ChartDataByWeekday{
-// 					"Sun": 100,
-// 					"Mon": 200,
-// 					"Tue": 300,
-// 				}
+				// only have data for Sun, Mon, Tue
+				chartDataByWeekday := domain.ChartDataByWeekday{
+					"Sun": 100,
+					"Mon": 200,
+					"Tue": 300,
+				}
 
-// 				s.mockTransaction.On("GetBarChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
-// 					Return(chartDataByWeekday, nil).Once()
-// 			},
-// 			chartDateRange: domain.ChartDateRange{
-// 				StartDate: "2024-03-17",
-// 				EndDate:   "2024-03-23",
-// 			},
-// 			transactionType: domain.TransactionTypeExpense,
-// 			user: domain.User{
-// 				ID: 1,
-// 			},
-// 			expResult: domain.ChartData{
-// 				Labels:   []string{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
-// 				Datasets: []float64{100, 200, 300, 0, 0, 0, 0},
-// 			},
-// 			expErr: nil,
-// 		},
-// 		{
-// 			desc: "when get chart data fail, return error",
-// 			setupFun: func() {
-// 				chartDataRange := domain.ChartDateRange{
-// 					StartDate: "2024-03-17",
-// 					EndDate:   "2024-03-23",
-// 				}
+				s.mockTransaction.On("GetBarChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
+					Return(chartDataByWeekday, nil).Once()
+			},
+			chartDateRange: domain.ChartDateRange{
+				StartDate: "2024-03-17",
+				EndDate:   "2024-03-23",
+			},
+			transactionType: domain.TransactionTypeExpense,
+			user: domain.User{
+				ID: 1,
+			},
+			expResult: domain.ChartData{
+				Labels:   []string{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"},
+				Datasets: []float64{100, 200, 300, 0, 0, 0, 0},
+			},
+			expErr: nil,
+		},
+		{
+			desc: "when get chart data fail, return error",
+			setupFun: func() {
+				chartDataRange := domain.ChartDateRange{
+					StartDate: "2024-03-17",
+					EndDate:   "2024-03-23",
+				}
 
-// 				s.mockTransaction.On("GetBarChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
-// 					Return(nil, errors.New("error")).Once()
-// 			},
-// 			chartDateRange: domain.ChartDateRange{
-// 				StartDate: "2024-03-17",
-// 				EndDate:   "2024-03-23",
-// 			},
-// 			transactionType: domain.TransactionTypeExpense,
-// 			user: domain.User{
-// 				ID: 1,
-// 			},
-// 			expResult: domain.ChartData{},
-// 			expErr:    errors.New("error"),
-// 		},
-// 	}
+				s.mockTransaction.On("GetBarChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
+					Return(nil, errors.New("error")).Once()
+			},
+			chartDateRange: domain.ChartDateRange{
+				StartDate: "2024-03-17",
+				EndDate:   "2024-03-23",
+			},
+			transactionType: domain.TransactionTypeExpense,
+			user: domain.User{
+				ID: 1,
+			},
+			expResult: domain.ChartData{},
+			expErr:    errors.New("error"),
+		},
+	}
 
-// 	for _, t := range tests {
-// 		s.Run(t.desc, func() {
-// 			s.SetupTest()
-// 			t.setupFun()
+	for _, t := range tests {
+		s.Run(t.desc, func() {
+			s.SetupTest()
+			t.setupFun()
 
-// 			result, err := s.transactionUC.GetBarChartData(mockCtx, t.chartDateRange, t.transactionType, t.user)
-// 			s.Require().Equal(t.expResult, result)
-// 			s.Require().Equal(t.expErr, err)
+			result, err := s.transactionUC.GetBarChartData(mockCtx, t.chartDateRange, t.transactionType, t.user)
+			s.Require().Equal(t.expResult, result)
+			s.Require().Equal(t.expErr, err)
 
-// 			s.TearDownTest()
-// 		})
-// 	}
-// }
+			s.TearDownTest()
+		})
+	}
+}
 
-// func (s *TransactionSuite) TestGetPieChartData() {
-// 	tests := []struct {
-// 		desc            string
-// 		setupFun        func()
-// 		chartDateRange  domain.ChartDateRange
-// 		transactionType domain.TransactionType
-// 		user            domain.User
-// 		expResult       domain.ChartData
-// 		expErr          error
-// 	}{
-// 		{
-// 			desc: "when no error, return chart data",
-// 			setupFun: func() {
-// 				chartDataRange := domain.ChartDateRange{
-// 					StartDate: "2024-03-17",
-// 					EndDate:   "2024-03-23",
-// 				}
+func (s *TransactionSuite) TestGetPieChartData() {
+	tests := []struct {
+		desc            string
+		setupFun        func()
+		chartDateRange  domain.ChartDateRange
+		transactionType domain.TransactionType
+		user            domain.User
+		expResult       domain.ChartData
+		expErr          error
+	}{
+		{
+			desc: "when no error, return chart data",
+			setupFun: func() {
+				chartDataRange := domain.ChartDateRange{
+					StartDate: "2024-03-17",
+					EndDate:   "2024-03-23",
+				}
 
-// 				chartData := domain.ChartData{
-// 					Labels:   []string{"label1", "label2"},
-// 					Datasets: []float64{100, 200},
-// 				}
+				chartData := domain.ChartData{
+					Labels:   []string{"label1", "label2"},
+					Datasets: []float64{100, 200},
+				}
 
-// 				s.mockTransaction.On("GetPieChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
-// 					Return(chartData, nil).Once()
-// 			},
-// 			chartDateRange: domain.ChartDateRange{
-// 				StartDate: "2024-03-17",
-// 				EndDate:   "2024-03-23",
-// 			},
-// 			transactionType: domain.TransactionTypeExpense,
-// 			user: domain.User{
-// 				ID: 1,
-// 			},
-// 			expResult: domain.ChartData{
-// 				Labels:   []string{"label1", "label2"},
-// 				Datasets: []float64{100, 200},
-// 			},
-// 			expErr: nil,
-// 		},
-// 		{
-// 			desc: "when get chart data fail, return error",
-// 			setupFun: func() {
-// 				chartDataRange := domain.ChartDateRange{
-// 					StartDate: "2024-03-17",
-// 					EndDate:   "2024-03-23",
-// 				}
+				s.mockTransaction.On("GetPieChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
+					Return(chartData, nil).Once()
+			},
+			chartDateRange: domain.ChartDateRange{
+				StartDate: "2024-03-17",
+				EndDate:   "2024-03-23",
+			},
+			transactionType: domain.TransactionTypeExpense,
+			user: domain.User{
+				ID: 1,
+			},
+			expResult: domain.ChartData{
+				Labels:   []string{"label1", "label2"},
+				Datasets: []float64{100, 200},
+			},
+			expErr: nil,
+		},
+		{
+			desc: "when get chart data fail, return error",
+			setupFun: func() {
+				chartDataRange := domain.ChartDateRange{
+					StartDate: "2024-03-17",
+					EndDate:   "2024-03-23",
+				}
 
-// 				s.mockTransaction.On("GetPieChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
-// 					Return(domain.ChartData{}, errors.New("error")).Once()
-// 			},
-// 			chartDateRange: domain.ChartDateRange{
-// 				StartDate: "2024-03-17",
-// 				EndDate:   "2024-03-23",
-// 			},
-// 			transactionType: domain.TransactionTypeExpense,
-// 			user: domain.User{
-// 				ID: 1,
-// 			},
-// 			expResult: domain.ChartData{},
-// 			expErr:    errors.New("error"),
-// 		},
-// 	}
+				s.mockTransaction.On("GetPieChartData", mockCtx, chartDataRange, domain.TransactionTypeExpense, int64(1)).
+					Return(domain.ChartData{}, errors.New("error")).Once()
+			},
+			chartDateRange: domain.ChartDateRange{
+				StartDate: "2024-03-17",
+				EndDate:   "2024-03-23",
+			},
+			transactionType: domain.TransactionTypeExpense,
+			user: domain.User{
+				ID: 1,
+			},
+			expResult: domain.ChartData{},
+			expErr:    errors.New("error"),
+		},
+	}
 
-// 	for _, t := range tests {
-// 		s.Run(t.desc, func() {
-// 			s.SetupTest()
-// 			t.setupFun()
+	for _, t := range tests {
+		s.Run(t.desc, func() {
+			s.SetupTest()
+			t.setupFun()
 
-// 			result, err := s.transactionUC.GetPieChartData(mockCtx, t.chartDateRange, t.transactionType, t.user)
-// 			s.Require().Equal(t.expResult, result)
-// 			s.Require().Equal(t.expErr, err)
+			result, err := s.transactionUC.GetPieChartData(mockCtx, t.chartDateRange, t.transactionType, t.user)
+			s.Require().Equal(t.expResult, result)
+			s.Require().Equal(t.expErr, err)
 
-// 			s.TearDownTest()
-// 		})
-// 	}
-// }
+			s.TearDownTest()
+		})
+	}
+}
