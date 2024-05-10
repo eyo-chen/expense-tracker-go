eac023657459f128fc9bb9c2130f16da3849f41e
Eyo Chen <eyo.chen@amazingtalker.com>
2024-05-11 01:03:51 +0800

    feat: add helper function at subcategory/factory

--- internal/model/subcateg/factory.go
+++ internal/model/subcateg/factory.go
@@ -2,6 +2,7 @@ package subcateg
 
 import (
 	"database/sql"
+	"errors"
 
 	"github.com/OYE0303/expense-tracker-go/internal/domain"
 	"github.com/OYE0303/expense-tracker-go/internal/model/icon"
@@ -74,6 +75,69 @@ func (f *factory) InsertSubcategs(n int, ows ...SubCateg) ([]SubCateg, user.User
 	}
 
 	return ss, u, m, nil
+}
+
+// InsertSubcategsWithOneOrManyMainCateg inserts n subcategories with one or many main categories and one user.
+// The maincategIndex is the number of main categories to insert.
+// The subcategIndexes is the number of subcategories to insert for each main category.
+func (f *factory) InsertSubcategsWithOneOrManyMainCateg(maincategIndex int, subcategIndexes []int, ows ...maincateg.MainCateg) (map[int64][]SubCateg, []maincateg.MainCateg, user.User, error) {
+	// validate input
+	// maincategIndex and subcategIndexes must have the same length
+	// because the value of subcategIndexes[i] is the number of subcategories to insert for the main category with index i.
+	if maincategIndex != len(subcategIndexes) {
+		return nil, nil, user.User{}, errors.New("maincategIndex and subcategIndexes must have the same length")
+	}
+
+	// prepare main categories overwrites
+	maincategOWs := make([]maincateg.MainCateg, maincategIndex)
+	for i := 0; i < maincategIndex; i++ {
+		if ows != nil && i < len(ows) {
+			maincategOWs[i] = ows[i]
+		}
+
+		// set default type to expense if not set
+		if maincategOWs[i].Type == "" {
+			maincategOWs[i].Type = domain.TransactionTypeExpense.ToModelValue()
+		}
+	}
+
+	// prepare associations data
+	u := user.User{}
+	iconPtrList := make([]interface{}, maincategIndex)
+	for i := 0; i < maincategIndex; i++ {
+		iconPtrList[i] = &icon.Icon{}
+	}
+
+	// insert main categories
+	ms, _, err := f.maincateg.BuildList(maincategIndex).Overwrites(maincategOWs...).WithOne(&u).WithMany(iconPtrList...).InsertWithAss()
+	if err != nil {
+		return nil, nil, user.User{}, err
+	}
+
+	// prepare sub categories overwrites
+	subcategOWs := make([][]SubCateg, len(subcategIndexes))
+	for i := 0; i < len(subcategIndexes); i++ {
+		subcategOWs[i] = make([]SubCateg, subcategIndexes[i])
+		for j := 0; j < subcategIndexes[i]; j++ {
+			subcategOWs[i][j].MainCategID = ms[i].ID
+			subcategOWs[i][j].UserID = u.ID
+		}
+	}
+
+	// insert sub categories, and prepare result
+	result := map[int64][]SubCateg{}
+	for index, amount := range subcategIndexes {
+		ows := subcategOWs[index]
+		ss, err := f.subCateg.BuildList(amount).Overwrites(ows...).Insert()
+		if err != nil {
+			return nil, nil, user.User{}, err
+		}
+
+		mainCateg := ms[index]
+		result[mainCateg.ID] = ss
+	}
+
+	return result, ms, u, nil
 }
 
 func (f *factory) Reset() {
