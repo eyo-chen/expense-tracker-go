e9cc8e7e77d742278395bf10af78ef716869a12d
Eyo Chen <eyo.chen@amazingtalker.com>
2024-05-25 11:50:53 +0800

    refactor: shorten the code

--- internal/usecase/subcateg/subcateg.go
+++ internal/usecase/subcateg/subcateg.go
@@ -3,7 +3,6 @@ package subcateg
 import (
 	"github.com/OYE0303/expense-tracker-go/internal/domain"
 	"github.com/OYE0303/expense-tracker-go/internal/model/interfaces"
-	"github.com/OYE0303/expense-tracker-go/pkg/logger"
 )
 
 type SubCategUC struct {
@@ -28,23 +27,11 @@ func (s *SubCategUC) Create(categ *domain.SubCateg, userID int64) error {
 }
 
 func (s *SubCategUC) GetAll(userID int64) ([]*domain.SubCateg, error) {
-	categs, err := s.SubCateg.GetAll(userID)
-	if err != nil {
-		logger.Error("s.SubCateg.GetAll failed", "package", "usecase", "err", err)
-		return nil, err
-	}
-
-	return categs, nil
+	return s.SubCateg.GetAll(userID)
 }
 
 func (s *SubCategUC) GetByMainCategID(userID, mainCategID int64) ([]*domain.SubCateg, error) {
-	categs, err := s.SubCateg.GetByMainCategID(userID, mainCategID)
-	if err != nil {
-		logger.Error("s.SubCateg.GetByMainCategID failed", "package", "usecase", "err", err)
-		return nil, err
-	}
-
-	return categs, nil
+	return s.SubCateg.GetByMainCategID(userID, mainCategID)
 }
 
 func (s *SubCategUC) Update(categ *domain.SubCateg, userID int64) error {
@@ -67,10 +54,5 @@ func (s *SubCategUC) Update(categ *domain.SubCateg, userID int64) error {
 }
 
 func (s *SubCategUC) Delete(id int64) error {
-	if err := s.SubCateg.Delete(id); err != nil {
-		logger.Error("s.SubCateg.Delete failed", "package", "usecase", "err", err)
-		return err
-	}
-
-	return nil
+	return s.SubCateg.Delete(id)
 }
