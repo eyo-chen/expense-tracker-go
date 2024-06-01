a917c9ec4bfc807d0f5e8de6f97eacd1e970c80c
Eyo Chen <eyo.chen@amazingtalker.com>
2024-06-01 11:50:11 +0800

    feat: add GetInfo at handler

--- internal/handler/user/user.go
+++ internal/handler/user/user.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/OYE0303/expense-tracker-go/internal/domain"
 	"github.com/OYE0303/expense-tracker-go/internal/usecase/interfaces"
+	"github.com/OYE0303/expense-tracker-go/pkg/ctxutil"
 	"github.com/OYE0303/expense-tracker-go/pkg/errutil"
 	"github.com/OYE0303/expense-tracker-go/pkg/jsonutil"
 	"github.com/OYE0303/expense-tracker-go/pkg/logger"
@@ -98,6 +99,31 @@ func (u UserHandler) Login(w http.ResponseWriter, r *http.Request) {
 
 	resp := map[string]interface{}{
 		"token": token,
+	}
+	if err := jsonutil.WriteJSON(w, http.StatusOK, resp, nil); err != nil {
+		logger.Error("jsonutil.WriteJSON failed", "package", "handler", "err", err)
+		errutil.ServerErrorResponse(w, r, err)
+		return
+	}
+}
+
+func (u UserHandler) GetInfo(w http.ResponseWriter, r *http.Request) {
+	userCtx := ctxutil.GetUser(r)
+	user, err := u.User.GetInfo(userCtx.ID)
+	if err != nil {
+		if err == domain.ErrUserIDNotFound {
+			errutil.BadRequestResponse(w, r, err)
+			return
+		}
+
+		errutil.ServerErrorResponse(w, r, err)
+		return
+	}
+
+	resp := map[string]interface{}{
+		"id":    user.ID,
+		"name":  user.Name,
+		"email": user.Email,
 	}
 	if err := jsonutil.WriteJSON(w, http.StatusOK, resp, nil); err != nil {
 		logger.Error("jsonutil.WriteJSON failed", "package", "handler", "err", err)
