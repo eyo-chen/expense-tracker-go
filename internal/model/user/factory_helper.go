700de989b1991ae15ce60880a1ebd3954054a729
Eyo Chen <eyo.chen@amazingtalker.com>
2024-06-02 16:23:41 +0800

    fix: do not specify id when creating mock data

--- internal/model/user/factory_helper.go
+++ internal/model/user/factory_helper.go
@@ -4,7 +4,6 @@ import "fmt"
 
 func userBluePrint(i int, _ User) User {
 	return User{
-		ID:                int64(i),
 		Name:              fmt.Sprintf("name%d", i),
 		Email:             fmt.Sprintf("email%d@gmail.com", i),
 		IsSetInitCategory: false,
