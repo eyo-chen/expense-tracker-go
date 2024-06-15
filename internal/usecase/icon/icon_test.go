a2c5ad7e081d87d0c1a9a151e447d4d0da251c84
CHEN YI YOU <78901921+OYE0303@users.noreply.github.com>
2024-06-15 09:06:17 +0800

    test: add unit testing (#119)
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

---
+++ internal/usecase/icon/icon_test.go
@@ -0,0 +1,58 @@
+package icon
+
+import (
+	"testing"
+
+	"github.com/OYE0303/expense-tracker-go/internal/domain"
+	"github.com/OYE0303/expense-tracker-go/internal/usecase/interfaces"
+	"github.com/OYE0303/expense-tracker-go/mocks"
+	"github.com/OYE0303/expense-tracker-go/pkg/testutil"
+	"github.com/stretchr/testify/suite"
+)
+
+type IconSuite struct {
+	suite.Suite
+	iconUC        interfaces.IconUC
+	mockIconModel *mocks.IconModel
+}
+
+func TestIconSuite(t *testing.T) {
+	suite.Run(t, new(IconSuite))
+}
+
+func (s *IconSuite) SetupTest() {
+	s.mockIconModel = mocks.NewIconModel(s.T())
+	s.iconUC = NewIconUC(s.mockIconModel)
+}
+
+func (s *IconSuite) TearDownTest() {
+	s.mockIconModel.AssertExpectations(s.T())
+}
+
+func (s *IconSuite) TestList() {
+	for scenario, fn := range map[string]func(s *IconSuite, desc string){
+		"when no error, return icon list": list_NoError_ReturnIconList,
+	} {
+		s.Run(testutil.GetFunName(fn), func() {
+			s.SetupTest()
+			fn(s, scenario)
+			s.TearDownTest()
+		})
+	}
+}
+
+func list_NoError_ReturnIconList(s *IconSuite, desc string) {
+	// prepare mock data
+	mockIcons := []domain.Icon{
+		{ID: 1, URL: "http://test.com/1"},
+		{ID: 2, URL: "http://test.com/1"},
+	}
+
+	// mock the function
+	s.mockIconModel.On("List").Return(mockIcons, nil)
+
+	// test function
+	icons, err := s.iconUC.List()
+	s.Require().NoError(err)
+	s.Require().Equal(mockIcons, icons)
+}
