c4dec28bacf36f8c887ec9fbbc4833d5b2369110
Eyo Chen <eyo.chen@amazingtalker.com>
2024-08-04 15:12:51 +0800

    test: modify testing case

--- internal/usecase/icon/icon_test.go
+++ internal/usecase/icon/icon_test.go
@@ -1,19 +1,27 @@
 package icon
 
 import (
+	"context"
+	"errors"
 	"testing"
 
 	"github.com/eyo-chen/expense-tracker-go/internal/domain"
 	"github.com/eyo-chen/expense-tracker-go/internal/usecase/interfaces"
 	"github.com/eyo-chen/expense-tracker-go/mocks"
 	"github.com/eyo-chen/expense-tracker-go/pkg/testutil"
+	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
+)
+
+var (
+	mockCTX = context.Background()
 )
 
 type IconSuite struct {
 	suite.Suite
-	iconUC        interfaces.IconUC
-	mockIconModel *mocks.IconModel
+	iconUC           interfaces.IconUC
+	mockIconModel    *mocks.IconModel
+	mockRedisService *mocks.RedisService
 }
 
 func TestIconSuite(t *testing.T) {
@@ -22,7 +30,8 @@ func TestIconSuite(t *testing.T) {
 
 func (s *IconSuite) SetupTest() {
 	s.mockIconModel = mocks.NewIconModel(s.T())
-	s.iconUC = NewIconUC(s.mockIconModel, nil)
+	s.mockRedisService = mocks.NewRedisService(s.T())
+	s.iconUC = NewIconUC(s.mockIconModel, s.mockRedisService)
 }
 
 func (s *IconSuite) TearDownTest() {
@@ -32,6 +41,7 @@ func (s *IconSuite) TearDownTest() {
 func (s *IconSuite) TestList() {
 	for scenario, fn := range map[string]func(s *IconSuite, desc string){
 		"when no error, return icon list": list_NoError_ReturnIconList,
+		"when cache failed, return error": list_CacheFailed_ReturnError,
 	} {
 		s.Run(testutil.GetFunName(fn), func() {
 			s.SetupTest()
@@ -43,16 +53,34 @@ func (s *IconSuite) TestList() {
 
 func list_NoError_ReturnIconList(s *IconSuite, desc string) {
 	// prepare mock data
-	mockIcons := []domain.Icon{
+	mockIconsStr := `[{"id":1,"url":"http://test.com/1"},{"id":2,"url":"http://test.com/1"}]`
+	mockGetFun := mock.AnythingOfType("func() (string, error)")
+
+	// mock the function
+	s.mockRedisService.On("GetByFunc", mockCTX, "icons", mockGetFun).Return(mockIconsStr, nil)
+
+	// prepare expected result
+	expResp := []domain.Icon{
 		{ID: 1, URL: "http://test.com/1"},
 		{ID: 2, URL: "http://test.com/1"},
 	}
 
-	// mock the function
-	s.mockIconModel.On("List").Return(mockIcons, nil)
-
 	// test function
 	icons, err := s.iconUC.List()
 	s.Require().NoError(err)
-	s.Require().Equal(mockIcons, icons)
+	s.Require().Equal(expResp, icons)
+}
+
+func list_CacheFailed_ReturnError(s *IconSuite, desc string) {
+	// prepare mock data
+	mockGetFun := mock.AnythingOfType("func() (string, error)")
+	s.mockRedisService.On("GetByFunc", mockCTX, "icons", mockGetFun).Return("", errors.New("cache failed"))
+
+	// prepare expected result
+	expResp := []domain.Icon(nil)
+
+	// test function
+	icons, err := s.iconUC.List()
+	s.Require().EqualError(err, "cache failed")
+	s.Require().Equal(expResp, icons)
 }
