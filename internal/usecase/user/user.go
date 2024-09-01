56caf1a9569c97e3bf30b48aead2f1df5c1a3487
Eyo Chen <eyo.chen@amazingtalker.com>
2024-09-01 21:36:53 +0800

    feat: return token struct

--- internal/usecase/user/user.go
+++ internal/usecase/user/user.go
@@ -23,58 +23,78 @@ func New(u interfaces.UserModel, r interfaces.RedisService) *UC {
 	return &UC{user: u, redis: r}
 }
 
-func (u *UC) Signup(user domain.User) (string, error) {
+func (u *UC) Signup(ctx context.Context, user domain.User) (domain.Token, error) {
 	_, err := u.user.FindByEmail(user.Email)
 	if err != nil && err != domain.ErrEmailNotFound {
-		return "", err
+		return domain.Token{}, err
 	}
 	if err == nil {
-		return "", domain.ErrEmailAlreadyExists
+		return domain.Token{}, domain.ErrEmailAlreadyExists
 	}
 
 	passwordHash, err := auth.GenerateHashPassword(user.Password)
 	if err != nil {
 		logger.Error("auth.GenerateHashPassword failed", "package", packageName, "err", err)
-		return "", err
+		return domain.Token{}, err
 	}
 
 	if err := u.user.Create(user.Name, user.Email, passwordHash); err != nil {
-		return "", err
+		return domain.Token{}, err
 	}
 
 	userWithID, err := u.user.FindByEmail(user.Email)
 	if err != nil {
-		return "", err
+		return domain.Token{}, err
 	}
 
-	token, err := genJWTToken(userWithID)
+	accessToken, err := genJWTToken(userWithID)
 	if err != nil {
-		return "", err
+		return domain.Token{}, err
+	}
+	refreshToken, err := genRefreshToken()
+	if err != nil {
+		return domain.Token{}, err
 	}
 
-	return token, nil
+	// it's ok to fail
+	_ = u.redis.Set(ctx, hashToken(refreshToken), userWithID.Email, 7*24*time.Hour)
+
+	return domain.Token{
+		Access:  accessToken,
+		Refresh: refreshToken,
+	}, nil
 }
 
-func (u *UC) Login(user domain.User) (string, error) {
+func (u *UC) Login(ctx context.Context, user domain.User) (domain.Token, error) {
 	userByEmail, err := u.user.FindByEmail(user.Email)
 	if err != nil {
 		if err == domain.ErrEmailNotFound {
-			return "", domain.ErrAuthentication
+			return domain.Token{}, domain.ErrAuthentication
 		}
 
-		return "", err
+		return domain.Token{}, err
 	}
 
 	if !auth.CompareHashPassword(user.Password, userByEmail.Password_hash) {
-		return "", domain.ErrAuthentication
+		return domain.Token{}, domain.ErrAuthentication
 	}
 
 	token, err := genJWTToken(userByEmail)
 	if err != nil {
-		return "", err
+		return domain.Token{}, err
+	}
+	refreshToken, err := genRefreshToken()
+	if err != nil {
+		return domain.Token{}, err
 	}
 
-	return token, nil
+	// it's ok to fail
+	_ = u.redis.Set(ctx, hashToken(refreshToken), userByEmail.Email, 7*24*time.Hour)
+
+	return domain.Token{
+		Access:  token,
+		Refresh: refreshToken,
+	}, nil
 }
 
 func (u *UC) Token(ctx context.Context, refreshToken string) (domain.Token, error) {
