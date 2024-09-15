da165693d827b99f7f261811c2cc676d379ee29e
Eyo Chen <eyo.chen@amazingtalker.com>
2024-09-15 10:05:56 +0800

    fix: add json tag

--- internal/domain/icon.go
+++ internal/domain/icon.go
@@ -2,20 +2,20 @@ package domain
 
 // DefaultIcon contains default icon information
 type DefaultIcon struct {
-	ID  int64
-	URL string
+	ID  int64  `json:"id"`
+	URL string `json:"url"`
 }
 
 // UserIcon contains user icon information
 type UserIcon struct {
-	ID        int64
-	UserID    int64
-	ObjectKey string
+	ID        int64  `json:"id"`
+	UserID    int64  `json:"user_id"`
+	ObjectKey string `json:"object_key"`
 }
 
 // Icon contains icon information
 type Icon struct {
-	ID   int64
-	Type IconType
-	URL  string
+	ID   int64    `json:"id"`
+	Type IconType `json:"type"`
+	URL  string   `json:"url"`
 }
