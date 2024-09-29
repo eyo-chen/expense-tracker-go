db52e096525d58a0ce37a5830eb2a191e5a537e6
Eyo Chen <eyo.chen@amazingtalker.com>
2024-09-29 09:15:20 +0800

    feat: validate request input

--- pkg/validator/categ.go
+++ pkg/validator/categ.go
@@ -4,15 +4,17 @@ import "github.com/eyo-chen/expense-tracker-go/internal/domain"
 
 func (v *Validator) CreateMainCateg(categ *domain.MainCateg) bool {
 	v.Check(len(categ.Name) > 0, "name", "Name can't be empty")
-	v.Check(categ.Icon.ID >= 0, "icon_id", "Icon ID must be greater than 0")
 	v.Check(categ.Type.IsValid(), "type", "Type must be income or expense")
+	v.Check(categ.IconType.IsValid(), "icon_type", "Icon type must be default or custom")
+	v.Check(categ.IconData != "", "icon_data", "Icon data can't be empty")
 	return v.Valid()
 }
 
 func (v *Validator) UpdateMainCateg(categ *domain.MainCateg) bool {
 	v.Check(len(categ.Name) > 0, "name", "Name can't be empty")
-	v.Check(categ.Icon.ID >= 0, "icon_id", "Icon ID must be greater than 0")
 	v.Check(categ.Type.IsValid(), "type", "Type must be income or expense")
+	v.Check(categ.IconType.IsValid(), "icon_type", "Icon type must be default or custom")
+	v.Check(categ.IconData != "", "icon_data", "Icon data can't be empty")
 	return v.Valid()
 }
 
