abb2d08f5b0345eb0b7ec07c83b280e95266be53
CHEN YI YOU <zxcvbn1239104@gmail.com>
2024-10-06 14:24:49 +0800

    Feat/create user icon at handler (#159)
    * feat: add interface and mock
    
    * feat: create user icon at handler
    
    * test: add unit testing
    
    ---------
    
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

--- pkg/validator/usericon.go
+++ pkg/validator/usericon.go
@@ -5,3 +5,9 @@ func (v *Validator) GetPutObjectURL(fileName string) bool {
 	v.Check(len(fileName) > 0, "file_name", "File name can't be empty")
 	return v.Valid()
 }
+
+// Create validates the file name.
+func (v *Validator) Create(fileName string) bool {
+	v.Check(len(fileName) > 0, "file_name", "File name can't be empty")
+	return v.Valid()
+}
