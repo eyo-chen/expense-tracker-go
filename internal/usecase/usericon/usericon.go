28c8d5265fd924218d3aa9b496a23b9e8d7d4c6b
CHEN YI YOU <zxcvbn1239104@gmail.com>
2024-10-06 14:14:44 +0800

    Feat/create user icon at usecase (#158)
    * feat: create user icon at usecase
    
    * feat: add unit testing
    
    ---------
    
    Co-authored-by: Eyo Chen <eyo.chen@amazingtalker.com>

--- internal/usecase/usericon/usericon.go
+++ internal/usecase/usericon/usericon.go
@@ -5,27 +5,40 @@ import (
 	"fmt"
 	"time"
 
+	"github.com/eyo-chen/expense-tracker-go/internal/domain"
 	"github.com/eyo-chen/expense-tracker-go/internal/usecase/interfaces"
 )
 
 type UC struct {
 	s3 interfaces.S3Service
+	ui interfaces.UserIconRepo
 }
 
-func New(s3 interfaces.S3Service) *UC {
+func New(s3 interfaces.S3Service, ui interfaces.UserIconRepo) *UC {
 	return &UC{
 		s3: s3,
+		ui: ui,
 	}
 }
 
 func (u *UC) GetPutObjectURL(ctx context.Context, fileName string, userID int64) (string, error) {
 	objectKey := fmt.Sprintf("user_icons/%d/%s", userID, fileName)
 	ttl := 60 * time.Second
-
 	url, err := u.s3.PutObjectUrl(ctx, objectKey, int64(ttl.Seconds()))
 	if err != nil {
 		return "", err
 	}
 
 	return url, nil
+}
+
+func (u *UC) Create(ctx context.Context, fileName string, userID int64) error {
+	objectKey := fmt.Sprintf("user_icons/%d/%s", userID, fileName)
+
+	ui := domain.UserIcon{
+		UserID:    userID,
+		ObjectKey: objectKey,
+	}
+
+	return u.ui.Create(ctx, ui)
 }
--- internal/usecase/usericon/usericon_test.go
+++ internal/usecase/usericon/usericon_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/eyo-chen/expense-tracker-go/internal/domain"
 	"github.com/eyo-chen/expense-tracker-go/mocks"
 	"github.com/eyo-chen/expense-tracker-go/pkg/testutil"
 	"github.com/stretchr/testify/suite"
@@ -20,6 +21,7 @@ type UserIconSuite struct {
 	suite.Suite
 	uc            *UC
 	mockS3Service *mocks.S3Service
+	mockUserIcon  *mocks.UserIconRepo
 }
 
 func TestUserIconSuite(t *testing.T) {
@@ -28,11 +30,13 @@ func TestUserIconSuite(t *testing.T) {
 
 func (s *UserIconSuite) SetupTest() {
 	s.mockS3Service = mocks.NewS3Service(s.T())
-	s.uc = New(s.mockS3Service)
+	s.mockUserIcon = mocks.NewUserIconRepo(s.T())
+	s.uc = New(s.mockS3Service, s.mockUserIcon)
 }
 
 func (s *UserIconSuite) TearDownTest() {
 	s.mockS3Service.AssertExpectations(s.T())
+	s.mockUserIcon.AssertExpectations(s.T())
 }
 
 func (s *UserIconSuite) TestGetPutObjectURL() {
@@ -80,4 +84,58 @@ func getPutObjectURL_GetObjectUrlFailed_ReturnError(s *UserIconSuite, desc strin
 	url, err := s.uc.GetPutObjectURL(mockCTX, mockFileName, mockUserID)
 	s.Require().ErrorIs(err, mockErr, desc)
 	s.Require().Empty(url, desc)
+}
+
+func (s *UserIconSuite) TestCreate() {
+	for scenario, fn := range map[string]func(s *UserIconSuite, desc string){
+		"when no error, return success":    create_NoError_CreateSuccessfully,
+		"when create failed, return error": create_CreateFailed_ReturnError,
+	} {
+		s.Run(testutil.GetFunName(fn), func() {
+			s.SetupTest()
+			fn(s, scenario)
+			s.TearDownTest()
+		})
+	}
+}
+
+func create_NoError_CreateSuccessfully(s *UserIconSuite, desc string) {
+	// prepare mock data
+	mockFileName := "test.png"
+	mockUserID := int64(1)
+	mockObjectKey := fmt.Sprintf("user_icons/%d/%s", mockUserID, mockFileName)
+	mockUserIcon := domain.UserIcon{
+		UserID:    mockUserID,
+		ObjectKey: mockObjectKey,
+	}
+
+	// prepare mock service
+	s.mockUserIcon.On("Create", mockCTX, mockUserIcon).Return(nil)
+
+	// action
+	err := s.uc.Create(mockCTX, mockFileName, mockUserID)
+
+	// assertion
+	s.Require().NoError(err, desc)
+}
+
+func create_CreateFailed_ReturnError(s *UserIconSuite, desc string) {
+	// prepare mock data
+	mockFileName := "test.png"
+	mockUserID := int64(1)
+	mockObjectKey := fmt.Sprintf("user_icons/%d/%s", mockUserID, mockFileName)
+	mockUserIcon := domain.UserIcon{
+		UserID:    mockUserID,
+		ObjectKey: mockObjectKey,
+	}
+	mockErr := errors.New("create failed")
+
+	// prepare mock service
+	s.mockUserIcon.On("Create", mockCTX, mockUserIcon).Return(mockErr)
+
+	// action
+	err := s.uc.Create(mockCTX, mockFileName, mockUserID)
+
+	// assertion
+	s.Require().ErrorIs(err, mockErr, desc)
 }
